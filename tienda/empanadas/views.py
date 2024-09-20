b7efcd1482040ef563578a85f752f49ef0674ae2
LePeruvienn <koto91@free.fr>
2024-09-20 15:05:29 +0200

    Hola compa, ese el es push del la vista iingredient y emnapandas, que rico

--- tienda/empanadas/views.py
+++ tienda/empanadas/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from empanadas.models import Empanada
+from empanadas.models import Ingredient
 
 # Create your views here.
+
+# "/empanadas" request
+def empanadas(request):
+    lesEmpanadas = Empanada.objects.all()
+    return render(
+            request,
+            'empanadas/empanadas.html',
+            {'empanadas' : lesEmpanadas}
+            )
+
+# "/ingredients" request
+def ingredients(request):
+    lesIngredients = Ingredient.objects.all()
+    return render(
+            request,
+            'ingredients/ingredients.html',
+            {'ingredients' : lesIngredients}
+            )
--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from empanadas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path( 'empanadas/', views.empanadas),
+    path( 'ingredients/', views.ingredients),
 ]
