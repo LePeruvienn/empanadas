bda599623bf666835ec3902906efd8f498c04912
LePeruvienn <koto91@free.fr>
2024-09-27 15:18:35 +0200

    Hola chico, es el push del TP5

--- tienda/empanadas/views.py
+++ tienda/empanadas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from empanadas.models import Empanada
 from empanadas.models import Ingredient
+from empanadas.models import Composition
 
 # Create your views here.
 
@@ -21,3 +22,17 @@ def ingredients(request):
             'empanadas/ingredients.html',
             {'ingredients' : lesIngredients}
             )
+
+
+# "/ingredients" request
+def empanada(request, empanada_id):
+    laEmpanada = Empanada.objects.get( idEmpanada = empanada_id )
+    compo = Composition.objects.filter (empanada = empanada_id)
+    return render(
+            request,
+            'empanadas/empanada.html',
+            {
+                'empanada' : laEmpanada,
+                'composition' : compo 
+            }
+    )
--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -21,5 +21,6 @@ from empanadas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path( 'empanadas/', views.empanadas),
+    path( 'empanadas/<int:empanada_id>', views.empanada),
     path( 'ingredients/', views.ingredients),
 ]
