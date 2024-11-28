bddf6f6b029eced72f2685201fe9b2100c16f6aa
LePeruvienn <koto91@free.fr>
2024-11-28 09:46:26 +0100

    Halo este es el TP11  && git push

--- tienda/comptes/urls.py
+++ tienda/comptes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from comptes import views
 
-urlspatterns = [
+urlpatterns = [
 
-    path( 'login', auth_views.LoginView.as_view(template_name='comptes/login.html'), name='login')
+    path( 'login', auth_views.LoginView.as_view(template_name='comptes/login.html'), name='login'),
+    path( 'logout', views.deconnexion, name='logout'),
+    path( 'connexion', views.connexion),
 ]
