91fb87f8a0561b5d38bcf54b8cdccb6c8f191472
LePeruvienn <koto91@free.fr>
2024-12-12 16:56:24 +0100

    Este es el push final del TP15  && git push

--- tienda/paniers/urls.py
+++ tienda/paniers/urls.py
@@ -5,4 +5,8 @@ from paniers import views
 urlpatterns = [
     path('cart', views.afficherPanier),
     path('cart/<int:empanada_id>/buy/', views.ajouterEmpanadaAuPanier),
+    path('cart/<int:empanada_id>/delete/', views.retirerDuPanier),
+    path('cart/<int:empanada_id>/decrease/', views.retirerUneEmpanadaDuPanier),
+    path('cart/delete/', views.viderPanier),
+    path('cart/pay/', views.payerPanier),
 ]
--- tienda/paniers/views.py
+++ tienda/paniers/views.py
@@ -36,13 +36,95 @@ def ajouterEmpanadaAuPanier(request, empanada_id):
     empanada = Empanada.objects.get(idEmpanada=empanada_id)
     user = TiendaUser.objects.get(id=request.user.id)
     # On créer la commande ou on la récupère et on lui rajoute la empanada
-    panier, created = Commande.objects.get_or_create(user=user, payee=False, defaults={"prix": 0})
+    panier, _ = Commande.objects.get_or_create(user=user, payee=False, defaults={"prix": 0})
     panier.prix = panier.prix + empanada.prix
     panier.save()
     # On créer une ligne de la commande ou on la récupère
-    ligne, created = LigneCommande.objects.get_or_create(commande=panier, empanada=empanada, defaults={"quantite": 0, "prix": 0})
+    ligne, _ = LigneCommande.objects.get_or_create(commande=panier, empanada=empanada, defaults={"quantite": 0, "prix": 0})
     ligne.quantite = ligne.quantite + 1
     ligne.prix = ligne.prix + empanada.prix
     ligne.save()
     # On affiche le panier
     return redirect ('/cart')
+
+def retirerDuPanier(request, empanada_id):
+    # Si le user est pas connecté on le redirige vers son panier
+    if not request.user.is_authenticated:
+        return redirect ('/login')
+    # On récupère les données
+    empanada = Empanada.objects.get(idEmpanada=empanada_id)
+    user = TiendaUser.objects.get(id=request.user.id)
+    # On récupère la commande en cours du client
+    panier = None
+    panier = Commande.objects.get(user=user, payee=False)
+    # On arrête tout si le panier existe pas 
+    if panier is not None:
+        # On récupère la ligne du panier
+        ligne = None
+        ligne = LigneCommande.objects.get(commande=panier, empanada=empanada)
+        # On arrête tout si la ligne existe pas 
+        if ligne is not None:
+            ligne.delete()
+    # On affiche le panier
+    return redirect ('/cart')
+
+def retirerUneEmpanadaDuPanier(request, empanada_id):
+    # Si le user est pas connecté on le redirige vers son panier
+    if not request.user.is_authenticated:
+        return redirect ('/login')
+    # On récupère les données
+    empanada = Empanada.objects.get(idEmpanada=empanada_id)
+    user = TiendaUser.objects.get(id=request.user.id)
+    # On récupère la commande en cours du client
+    panier = None
+    panier = Commande.objects.get(user=user, payee=False)
+    # On arrête tout si le panier existe pas 
+    if panier is not None:
+        # On récupère la ligne du panier
+        ligne = None
+        ligne = LigneCommande.objects.get(commande=panier, empanada=empanada)
+        # On arrête tout si la ligne existe pas 
+        if ligne is not None:
+            if ligne.quantite > 1:
+                ligne.quantite = ligne.quantite - 1
+                ligne.prix = ligne.prix - empanada.prix
+                ligne.save()
+            else:
+                return retirerDuPanier(request, empanada_id)
+    # On affiche le panier
+    return redirect ('/cart')
+
+def viderPanier(request):
+    # Si le user est pas connecté on le redirige vers son panier
+    if not request.user.is_authenticated:
+        return redirect ('/login')
+    # On récupère les données
+    user = TiendaUser.objects.get(id=request.user.id)
+    # On récupère la commande en cours du client
+    panier = None
+    panier = Commande.objects.get(user=user, payee=False)
+    # On arrête tout si le panier existe pas 
+    if panier is not None:
+        panier.delete()
+    # On affiche le panier
+    return redirect('/cart')
+
+def payerPanier(request):
+    # Si le user est pas connecté on le redirige vers son panier
+    if not request.user.is_authenticated:
+        return redirect ('/login')
+    # On récupère les données
+    user = TiendaUser.objects.get(id=request.user.id)
+    # On récupère la commande en cours du client
+    panier = None
+    panier = Commande.objects.get(user=user, payee=False)
+    # On arrête tout si le panier existe pas 
+    if panier is not None:
+        panier.payee = True
+        panier.save()
+        return render (
+            request,
+            'paniers/avisPaiement.html',
+            {}
+        )
+    return redirect('/cart')
